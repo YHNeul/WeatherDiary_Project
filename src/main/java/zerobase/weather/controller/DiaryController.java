dcce70efa78f3078fdeb9bff762f7c15baee0465
9898s <sueh9898@icloud.com>
2023-07-23 06:00:33 +0900

    swagger 적용하기

--- src/main/java/zerobase/weather/controller/DiaryController.java
+++ src/main/java/zerobase/weather/controller/DiaryController.java
@@ -1,5 +1,7 @@
 package zerobase.weather.controller;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.*;
 import zerobase.weather.domain.Diary;
@@ -16,6 +18,7 @@ public class DiaryController {
         this.diaryService = diaryService;
     }
 
+    @ApiOperation(value = "일기 텍스트와 날씨를 이용해서 DB에 일기 저장", notes = "노트 영역")
     @PostMapping("/create/diary")
     void createDiary(
             @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date,
@@ -24,6 +27,7 @@ public class DiaryController {
         diaryService.createDiary(date, text);
     }
 
+    @ApiOperation("선택한 날짜의 모든 일기 데이터를 가져옵니다")
     @GetMapping("/read/diary")
     List<Diary> readDiary(
             @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date
@@ -31,10 +35,13 @@ public class DiaryController {
         return diaryService.readDiary(date);
     }
 
+    @ApiOperation("선택한 기간중의 모든 일기 데이터를 가져옵니다")
     @GetMapping("/read/diaries")
     List<Diary> readDiaries(
-            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate startDate,
-            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate endDate
+            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+            @ApiParam(value = "조회할 기간의 첫번째 날", example = "2020-02-02") LocalDate startDate,
+            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+            @ApiParam(value = "조회할 기간의 마지막 날", example = "2020-02-02") LocalDate endDate
     ) {
         return diaryService.readDiaries(startDate, endDate);
     }
