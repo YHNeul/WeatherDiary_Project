362d2716fc97aaa67db68d166456a71f62377daa
9898s <sueh9898@icloud.com>
2023-08-04 03:54:19 +0900

    피드백 수정

--- src/main/java/zerobase/weather/config/GlobalExceptionHandler.java
+++ src/main/java/zerobase/weather/config/GlobalExceptionHandler.java
@@ -1,16 +1,17 @@
 package zerobase.weather.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import zerobase.weather.dto.ErrorResponse;
 
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     @ExceptionHandler(Exception.class)
-    public Exception handleAllException() {
-        System.out.println("error from GlobalExceptionHandler");
-        return new Exception();
+    public ErrorResponse handleAllException(Exception exception) {
+        log.error("Error from GlobalExceptionHandler: ", exception);
+        return new ErrorResponse("", HttpStatus.INTERNAL_SERVER_ERROR.value());
     }
 }
--- src/main/java/zerobase/weather/service/DiaryService.java
+++ src/main/java/zerobase/weather/service/DiaryService.java
@@ -1,48 +1,38 @@
 package zerobase.weather.service;
 
+import lombok.RequiredArgsConstructor;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Transactional;
-import zerobase.weather.WeatherApplication;
 import zerobase.weather.domain.DateWeather;
 import zerobase.weather.domain.Diary;
+import zerobase.weather.error.InvalidDateException;
 import zerobase.weather.repository.DateWeatherRepository;
 import zerobase.weather.repository.DiaryRepository;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.time.LocalDate;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 @Service
+@RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class DiaryService {
-    @Value("${openweathermap.key}") // application.properties 값을 가져옴
-    private String apiKey;
-
     private final DiaryRepository diaryRepository;
     private final DateWeatherRepository dateWeatherRepository;
-    private static final Logger logger = LoggerFactory.getLogger(WeatherApplication.class);
 
-    public DiaryService(DiaryRepository diaryRepository, DateWeatherRepository dateWeatherRepository) {
-        this.diaryRepository = diaryRepository;
-        this.dateWeatherRepository = dateWeatherRepository;
-    }
+    private final ApiService apiService;
+
+    private static final Logger logger = LoggerFactory.getLogger(DiaryService.class);
 
     @Transactional
-    @Scheduled(cron = "0 0 1 * * *")
     public void saveWeatherDate() {
         dateWeatherRepository.save(getWeatherFromApi());
     }
@@ -66,9 +56,9 @@ public class DiaryService {
 
     @Transactional(readOnly = true)
     public List<Diary> readDiary(LocalDate date) {
-//        if (date.isAfter(LocalDate.ofYearDay(3050, 1))) {
-//            throw new InvalidDate();
-//        }
+        if (date.isAfter(LocalDate.ofYearDay(2050, 1))) {
+            throw new InvalidDateException();
+        }
         return diaryRepository.findAllByDate(date);
     }
 
@@ -89,7 +79,7 @@ public class DiaryService {
 
     private DateWeather getDateWeather(LocalDate date) {
         List<DateWeather> dateWeatherListFromDB = dateWeatherRepository.findAllByDate(date);
-        if (dateWeatherListFromDB.size() == 0) {
+        if (dateWeatherListFromDB.isEmpty()) {
             // 새로 api에서 날씨 정보를 가져와야 한다.
             return getWeatherFromApi();
         } else {
@@ -99,7 +89,7 @@ public class DiaryService {
 
     private DateWeather getWeatherFromApi() {
         // openWeatherMap에서 날씨 데이터 가져오기
-        String weatherData = getWeatherString();
+        String weatherData = apiService.getWeatherString();
 
         // 받아온 날씨 json 파싱하기
         Map<String, Object> parsedWeather = parseWeather(weatherData);
@@ -110,35 +100,6 @@ public class DiaryService {
         dateWeather.setIcon(parsedWeather.get("icon").toString());
         dateWeather.setTemperature((Double) parsedWeather.get("temp"));
         return dateWeather;
-    }
-
-    private String getWeatherString() {
-        String apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=seoul&appid=" + apiKey;
-
-        try {
-            URL url = new URL(apiUrl);
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-            connection.setRequestMethod("GET");
-            BufferedReader br;
-
-            int responseCode = connection.getResponseCode();
-            if (responseCode == 200) {
-                br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-            } else {
-                br = new BufferedReader(new InputStreamReader(connection.getErrorStream()));
-            }
-
-            String inputLine;
-            StringBuilder response = new StringBuilder();
-            while ((inputLine = br.readLine()) != null) {
-                response.append(inputLine);
-            }
-            br.close();
-
-            return response.toString();
-        } catch (Exception e) {
-            return "failed to get response";
-        }
     }
 
     private Map<String, Object> parseWeather(String jsonString) {
